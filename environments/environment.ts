0af031295a865c5b0b5c80f48281aed1dce615f9
Kavithra9 <133741206+Kavithra9@users.noreply.github.com>
2023-05-19 11:44:53 +0530

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,45 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+   firebase: {
+    apiKey: "AIzaSyBTkOsUKZO0ZvTjuLVNF7D44--zJ4QxPDQ",
+    authDomain: "gem-app-c1c9b.firebaseapp.com",
+     databaseURL: "https://gem-app-c1c9b.firebaseio.com",
+     projectId: "gem-app-c1c9b",
+     storageBucket: "gem-app-c1c9b.appspot.com",
+    messagingSenderId: "770145745987",
+     appId: "1:770145745987:web:6b4b3dcf5650748ee528f6"
+   }
+
+
+  // firebase: {
+  //   apiKey: 'AIzaSyCRDKNceshus7b-z0ynoUEaTYU43NedSuA',
+  //   authDomain: 'gemshop-2b767.firebaseapp.com',
+  //   databaseURL: 'https://gemshop-2b767.firebaseio.com',
+  //   projectId: 'gemshop-2b767',
+  //   storageBucket: 'gemshop-2b767.appspot.com',
+  //   messagingSenderId: '638013485545',
+  //   appId: '1:638013485545:web:220dca8b2754093dbe9b78',
+  //   measurementId: 'G-YCL8TW41TZ'
+  // }
+
+
+
+
+
+
+
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
